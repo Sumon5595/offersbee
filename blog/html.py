92e8188b00ac1a849c383575325b763a8948816d
smon14300 <79419547+smon14300@users.noreply.github.com>
2021-08-16 20:46:55 +0200

    update

--- blog/html.py
+++ blog/html.py
@@ -4,7 +4,7 @@ filename = "".join(str(address)+".html")
 
 data = """<head><meta http-equiv="refresh" content="1;url=https://bdjob24.org/?blog="""
 data1 = address
-data2 = """/><meta name="robots" content="noindex"><meta name="googlebot" content="noindex"></head><body><p style="color:red; font-size:40px;">Malubd.Org - Download Link Loading...</p></body>"""
+data2 = """" /><meta name="robots" content="noindex"><meta name="googlebot" content="noindex"></head><body><p style="color:red; font-size:40px;">Malubd.Org - Download Link Loading...</p></body>"""
 final = data+data1+data2
 
 f= open(filename,"a")
