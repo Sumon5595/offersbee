0221559fb53ab38910a2aa6bcf18b25b808230b7
smon14300 <79419547+smon14300@users.noreply.github.com>
2021-08-16 20:37:50 +0200

    Update html.py

--- blog/html.py
+++ blog/html.py
@@ -3,7 +3,7 @@ address = input()
 filename = "".join(str(address)+".html")
 
 data = """<head><meta http-equiv="refresh" content="1;url=https://bdjob24.org/?blog="""
-data1 = filename
+data1 = address
 data2 = """/><meta name="robots" content="noindex"><meta name="googlebot" content="noindex"></head><body><p style="color:red; font-size:40px;">Malubd.Org - Download Link Loading...</p></body>"""
 final = data+data1+data2
 
