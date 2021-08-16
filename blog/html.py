bd1eb46d765bf22d5f15157cce4013ceb441af13
smon14300 <79419547+smon14300@users.noreply.github.com>
2021-08-16 20:22:15 +0200

    update

---
+++ blog/html.py
@@ -0,0 +1,13 @@
+address = input()
+
+filename = "".join(str(address)+".html")
+
+data = """<head><meta http-equiv="refresh" content="1;url=https://bdjob24.org/?blog="""
+data1 = filename
+data2 = """/><meta name="robots" content="noindex"><meta name="googlebot" content="noindex"></head><body><p style="color:red; font-size:40px;">Malubd.Org - Download Link Loading...</p></body>"""
+final = data+data1+data2
+
+f= open(filename,"a")
+f.write(f'{final}\n')
+print(filename)
+
